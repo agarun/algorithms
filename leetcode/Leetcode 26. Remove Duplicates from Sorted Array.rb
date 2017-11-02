e3929c4443a38e57de5164f6d5937ccae427e863
agarun <19801205+agarun@users.noreply.github.com>
2017-11-02 00:20:11 -0400

    Update Leetcode problem 26

--- leetcode/Leetcode 26. Remove Duplicates from Sorted Array.rb
+++ leetcode/Leetcode 26. Remove Duplicates from Sorted Array.rb
@@ -12,42 +12,70 @@
 # with the first two elements of nums being 1 and 2 respectively.
 # It doesn't matter what you leave beyond the new length.
 
+# O(n) time complexity
 def remove_duplicates(nums)
   return if nums.empty?
 
+  # `i` is a counter that increments when elements are unique
+  # `j` is the index in the array, it can start at 1 since i is first compared at 0
   i = 0
   j = 1
 
   while j < nums.size
     if nums[i] != nums[j]
-      # if these els aren't equal, they are uniques!
       i += 1
 
-      # set i to point to the new unique & discard the repeat
+      # i should point to the most recent unique for the next comparison
+      # therefore `i + 1` (note `i` increments just before) should have `j`'s current value
       nums[i] = nums[j]
     end
 
     j += 1
   end
 
-  i + 1 # + 1 to account for the j
+  # add 1 to include the last value checked, which will never be compared as `i`
+  i + 1
 end
 
-p remove_duplicates([1, 1, 2])
+p remove_duplicates([1, 1, 2]) # => 2
 
+# equivalent
 def remove_duplicates(nums)
   return if nums.empty?
 
-  catchup = 0
+  count = 0
 
   (1...nums.size).each do |index|
-    next if nums[catchup] == nums[index]
+    next if nums[count] == nums[index]
 
-    catchup += 1
-    nums[catchup] = nums[index]
+    count += 1
+    nums[count] = nums[index]
   end
 
-  catchup + 1
+  count + 1
 end
 
-p remove_duplicates([1, 1, 2])
+p remove_duplicates([1, 1, 2]) # => 2
+
+# to actually remove duplicate elements, adjust the first implementation
+# to build a new array of uniques, or alternatively:
+def remove_duplicates(nums)
+  i = 0
+  j = 1
+
+  while j < nums.size
+    if nums[i] == nums[j]
+      # found a repeat -> cut away the duplicate + reset the search
+      nums = j == nums.size - 1 ? nums[0..i] : nums[j..-1]
+      i = 0
+      j = 1
+    else
+      i += 1
+      j += 1
+    end
+  end
+
+  nums.size
+end
+
+p remove_duplicates([1, 1, 2]) # => 2
